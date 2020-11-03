2e933805a521ec18f159b301b94c29de9b33f4d1
ChroMari <chromarija@gmail.com>
2020-11-03 16:29:18 +0300

    feat: to-do app and server json

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as AddList } from './AddListButton/index';
+export { default as Badge } from './Badge/index';
+export { default as List } from './List/index';
+export { default as Tasks } from './Tasks/index';
